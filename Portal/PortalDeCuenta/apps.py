a761e67a425867a1c2bddee5917c6efb803cf328
Giovanni Peirone <mgpeirone@escuelasproa.edu.ar>
2024-08-12 19:48:45 -0300

    a

--- Portal/PortalDeCuenta/apps.py
+++ Portal/PortalDeCuenta/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class RegisterConfig(AppConfig):
+class PortaldecuentaConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'register'
+    name = 'PortalDeCuenta'
