12f6a7185571c5aa7256cd9b21cac34669df81a1
Gio <134148500+GiovanniPeirone@users.noreply.github.com>
2024-08-12 19:11:22 -0300

    Delete Portal directory

--- Portal/Usuarios/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Usuarios(models.Model):
-    id = models.BigIntegerField(auto_created=True, primary_key=True, null=False)
-    Nombre = models.CharField(max_length=255)
-    Email = models.CharField(max_length=255)
-    Contraseña = models.CharField(max_length=255)
