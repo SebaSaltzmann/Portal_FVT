1ed9b925f88bb29691337261507f3e3b231a5dd4
ludmilaramonda <145378638+ludmilaramonda@users.noreply.github.com>
2024-10-16 10:43:05 -0300

    .

--- ui/login.js
+++ ui/login.js
@@ -1,4 +1,3 @@
-function iniciarSesion() 
-{
-    window.location.href = "C:\Users\Proa\Documents\GitHub\Portal_FVT\ui\login.html"; 
-}
+function irAPagina() {
+    window.location.href = "C:/Users/Proa/Documents/GitHub/Portal_FVT/ui/login.html"; // Cambia esto por la URL de tu página de destino
+}nb 
